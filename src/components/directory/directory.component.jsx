ecfbc58bdd0c22fb0de4f905d67b20d3e19179b7
Taja House <ahlliahtaja@yahoo.com>
2020-05-04 08:51:17 -0700

    Broken homepage into smaller components and finished styling the menu-item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import MenuItem from "../menu-item/menu-item.component";
 import "../directory/dist/directory.style.css";
 
